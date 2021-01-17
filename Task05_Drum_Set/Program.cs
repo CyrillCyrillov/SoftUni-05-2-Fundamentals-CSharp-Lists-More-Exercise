a7831c6b79f2a58e74a1c86f84dd049a8985e83f
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 20:01:24 +0200

    Add files via upload

---
+++ Task05_Drum_Set/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task05_Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saveMoney = double.Parse(Console.ReadLine());
+            int[] drumSetBegin = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            List<int> drumSet = drumSetBegin.ToList();
+
+            while (true)
+            {
+                string comand = Console.ReadLine();
+                if(comand.ToUpper() == "HIT IT AGAIN, GABSY!")
+                {
+                    break;
+                }
+
+                int hitpower = int.Parse(comand);
+
+                for (int i = 0; i <= drumSet.Count - 1; i++)
+                {
+                    if(drumSet[i] > 0)
+                    {
+                        drumSet[i] -= hitpower;
+                        if (drumSet[i] <= 0)
+                        {
+                            double neededMoney = drumSetBegin[i] * 3;
+                            if (neededMoney <= saveMoney)
+                            {
+                                drumSet[i] = drumSetBegin[i];
+                                saveMoney -= neededMoney;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+
+            for (int i = 0; i <= drumSet.Count - 1; i++)
+            {
+                if(drumSet[i] <= 0)
+                {
+                    drumSet.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+            Console.WriteLine(string.Join(' ', drumSet));
+            Console.WriteLine($"Gabsy has {saveMoney:f2}lv.");
+        }
+    }
+}
