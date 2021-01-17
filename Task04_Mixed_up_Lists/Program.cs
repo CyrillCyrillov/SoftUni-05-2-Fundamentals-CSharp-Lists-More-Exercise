49dbbc8a201e478edab1ab6fffdc6d1c5c285702
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:59:18 +0200

    Add files via upload

---
+++ Task04_Mixed_up_Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task04_Mixed_up_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> leftList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> rightList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            List<int> mixedList = new List<int>(leftList.Count + rightList.Count - 2);
+
+            while (leftList.Count > 2 || rightList.Count > 2)
+            {
+                mixedList.Add(leftList[0]);
+                leftList.RemoveAt(0);
+
+                mixedList.Add(rightList[rightList.Count - 1]);
+                int helpIndex = rightList.Count - 1;
+                rightList.RemoveAt(helpIndex);
+            }
+
+            int startNumber = int.MinValue;
+            int endNumber = int.MaxValue;
+
+
+            if(leftList.Count == 0)
+            {
+                startNumber = rightList.Min();
+                endNumber = rightList.Max();
+            }
+            else
+            {
+                startNumber = leftList.Min();
+                endNumber = leftList.Max();
+            }
+
+            mixedList.RemoveAll(x => x <= startNumber);
+            mixedList.RemoveAll(x => x >= endNumber);
+
+
+            mixedList.Sort();
+
+            Console.WriteLine(string.Join(' ', mixedList));
+        }
+    }
+}
